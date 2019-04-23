742bdffcd92ca8915d30e96d756401c2f9502e7e
Daniel Gamez <acocolito@gmail.com>
2019-04-23 12:31:23 -0700

    Tests

---
+++ Start/Minutes/MinutesTests/MinutesTests.m
@@ -0,0 +1,25 @@
+//
+//  MinutesTests.m
+//  MinutesTests
+//
+//  Created by Daniel Gamez Navarro on 4/23/19.
+//  Copyright © 2019 Microsoft. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MinutesTests : XCTestCase
+import statement @testable import Minutes
+@end
+
+@implementation MinutesTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+@end
---
+++ Start/Minutes/MinutesTestsNoIse/MinutesTestsNoIse.swift
@@ -0,0 +1,32 @@
+//
+//  MinutesTestsNoIse.swift
+//  MinutesTestsNoIse
+//
+//  Created by Daniel Gamez Navarro on 4/23/19.
+//  Copyright © 2019 Microsoft. All rights reserved.
+//
+
+import XCTest
+@testable import Minutes
+class MinutesTestsNoIse: XCTestCase {
+
+    var entryUnderTest: Entry!
+    
+    
+    override func setUp() {
+        entryUnderTest = Entry("Title", "Content")
+    }
+
+    override func tearDown() {
+        entryUnderTest = nil
+    }
+
+    func testSerialization() {
+        let serializedData = FileEntryStore.serialize(entryUnderTest)
+        let entry = FileEntryStore.deserialize(serializedData!)
+        
+        XCTAssertEqual(entry?.title, "Title", "Title does not match")
+        XCTAssertEqual(entry?.content, "Content", "Content does not match")
+    }
+    
+}
